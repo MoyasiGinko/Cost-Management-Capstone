a0b486c8b1e0bfdacc0735b64c7f81fed9f14287
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-21 17:47:10 +0600

    Deployment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Enable server timing
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -26,7 +24,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
